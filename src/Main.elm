7953bf3f5fbf437460bbb59e55efcf174160724f
Jack <jackhp95@gmail.com>
2018-10-06 14:20:01 -0500

    progress, yexting you back works

--- src/Main.elm
+++ src/Main.elm
@@ -36,10 +36,24 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div [ class "w-100 vh-100 flex flex-wrap items-center justify-center" ]
-        [ img [ class "w-60 center", src "/logo.png" ] []
-        , h1 [ class "purple tc" ] [ text "Welcome to 48Below" ]
-        , a [ href "sms:15733408423;?&body=48below", class "bg-purple flex-auto tc link pa3 ma3 br2 white grow f4" ]
-            [ text "Find events" ]
+        [ img [ class "w-60 center", src "logo.png" ] []
+        , h1 [ class "purple tc" ]
+            [ text "Welcome to 48Below" ]
+        , div
+            [ class "dn-ns" ]
+            [ a [ href "sms:15738251289;?&body=48below", class "bg-purple flex-auto tc link pa3 ma3 br2 white grow f4" ]
+                [ text "Find events" ]
+            ]
+        , div
+            [ class "dn dib-ns" ]
+            [ text "recieve a text from us! "
+            , input
+                [ type_ "tel", placeholder "Mobile Number" ]
+                []
+            , button
+                [ class "button-reset bn pa3 ma0 bg-purple white" ]
+                [ text "Get Started!" ]
+            ]
         ]
 
 
